560610b12f4dd126e82bbe4dc381fabd141f83b2
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-09-10 17:46:28 +0900

    ADD  API環境構築(途中)

--- scraping.py
+++ scraping.py
@@ -26,7 +26,7 @@ def fetch_portal(id, password, max_page):
     """
 
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     driver.get("https://lc.okiu.ac.jp/portal/")
     id_el = driver.find_element_by_id('userID')
@@ -41,7 +41,7 @@ def fetch_portal(id, password, max_page):
     driver.switch_to.window(window)       # 遷移先のウィンドウに遷移元のウィンドウ情報を移す
 
     time.sleep(3)
-    info_click = driver.find_element_by_id('newsViewMore').click()
+    driver.find_element_by_id('newsViewMore').click()
     time.sleep(1)
     info_container = driver.find_element_by_id('tbl_news')
     info_pages = info_container.find_elements_by_tag_name('tr')
