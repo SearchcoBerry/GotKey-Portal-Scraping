b41451a01d98d3a180a3ee8b783f2c5988814734
helloworld753315 <helloworld.cpp.h@gmail.com>
2023-02-15 20:34:34 +0900

    Modified Selenium4での動作に対応

--- scraping.py
+++ scraping.py
@@ -1,6 +1,8 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -25,37 +27,31 @@ def fetch_portal(id, password, max_page):
     """
 
     options = Options()
-    options.add_argument('--headless')
-    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
-
-    driver = webdriver.Remote(
-        command_executor=os.environ["SELENIUM_URL"],
-        desired_capabilities=DesiredCapabilities.CHROME.copy(),
-        chrome_options=options
-    )
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     driver.get("https://lc.okiu.ac.jp/portal/")
-    id_el = driver.find_element_by_id('userID')
+    id_el = driver.find_element(By.ID, 'userID')
     id_el.send_keys(id)
-    password_el = driver.find_element_by_id('password')
+    password_el = driver.find_element(By.ID, 'password')
     password_el.send_keys(password)
-    submit = driver.find_element_by_class_name('btn_login')
+    submit = driver.find_element(By.CLASS_NAME, 'btn_login')
     submit.click()
 
-    WebDriverWait(driver, 3).until(lambda d: len(d.window_handles) > 1)
+    WebDriverWait(driver, 4).until(lambda d: len(d.window_handles) > 1)
     window = driver.window_handles[-1]   # ウィンドウ情報を取得
     driver.switch_to.window(window)       # 遷移先のウィンドウに遷移元のウィンドウ情報を移す
 
-    time.sleep(3)
-    driver.find_element_by_id('newsViewMore').click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'newsViewMore')))
+    driver.find_element(By.ID, 'newsViewMore').click()
     time.sleep(1)
-    info_container = driver.find_element_by_id('tbl_news')
-    info_pages = info_container.find_elements_by_tag_name('tr')
+    info_container = driver.find_element(By.ID, 'tbl_news')
+    info_pages = info_container.find_elements(By.TAG_NAME, 'tr')
 
     for i in range(len(info_pages)):
         if int(max_page) > i:
-            date = info_pages[i].find_element_by_class_name('day').text
-            title_container = info_pages[i].find_element_by_class_name('title')
-            title = title_container.find_element_by_tag_name('a').text
+            date = info_pages[i].find_element(By.CLASS_NAME, 'day').text 
+            title_container = info_pages[i].find_element(By.CLASS_NAME, 'title')
+            title = title_container.find_element(By.TAG_NAME, 'a').text 
             print(f"{i}: {date} {title}")
 
     driver.quit()
