b812c466c4d7931518502aaef372a055e33b2b3f
helloworld753315 <helloworld.cpp.h@gmail.com>
2023-02-21 18:41:02 +0900

    FIXED WebDriverManagement削除

--- scraping.py
+++ scraping.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
